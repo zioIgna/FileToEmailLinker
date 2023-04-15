11451e18d6128edbe2bbbfc3d0a5bc2087db7e57
Ignazio Carbonaro <ignaziocarbonaro@hotmail.com>
2023-04-15 23:09:59 +0200

    First commit

---
+++ FileToEmailLinker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileToEmailLinker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
