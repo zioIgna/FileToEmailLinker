39606bdc40f40786041d2c86c02e8a54e1003596
Ignazio Carbonaro <ignaziocarbonaro@hotmail.com>
2023-06-18 17:56:17 +0200

    Receiver model class added

---
+++ FileToEmailLinker/Models/Receiver.cs
@@ -0,0 +1,10 @@
+﻿namespace FileToEmailLinker.Models
+{
+    public class Receiver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
