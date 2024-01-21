3aeed80ec0b41538b00211729094d566a141b146
Ignazio Carbonaro <ignaziocarbonaro@hotmail.com>
2024-01-21 18:26:12 +0100

    Reso nullable il campo Date della schedulazione

--- FileToEmailLinker/Models/Entities/Schedulation.cs
+++ FileToEmailLinker/Models/Entities/Schedulation.cs
@@ -6,15 +6,14 @@ namespace FileToEmailLinker.Models.Entities
     public class Schedulation
     {
         public int Id { get; set; }
-        public string Name { get; set; } = DateTime.Now.ToString();
+        public string Name { get; set; } // = DateTime.Now.ToString();
         public SchedulingRecurrence Recurrence { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateOnly Date { get; set; }
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateOnly? Date { get; set; }
         public TimeOnly Time { get; set; }
         public DateOnly StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
         public DateOnly EndDate { get; set; } = DateOnly.MaxValue;
-        //ICollection<MailingPlan> MailingPlanList { get; set; } = new List<MailingPlan>();
         public bool Monday { get; set; }
         public bool Tuesday { get; set; }
         public bool Wednesday { get; set; }
