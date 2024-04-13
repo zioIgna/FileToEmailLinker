a8c5857b5f6d63248c9f19bd7cf6fe1f6566ae96
Ignazio Carbonaro <ignaziocarbonaro@hotmail.com>
2024-04-13 16:25:25 +0200

    Migration creazione entità Alert

---
+++ FileToEmailLinker/Migrations/20240413135121_AlertEntityAdded.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileToEmailLinker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlertEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FixedDatesSchedulation_MailingPlanId",
+                table: "FixedDatesSchedulation");
+
+            migrationBuilder.CreateTable(
+                name: "Alert",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Message = table.Column<string>(type: "TEXT", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    References = table.Column<string>(type: "TEXT", nullable: true),
+                    AlertSeverity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Visualized = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alert", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FixedDatesSchedulation_MailingPlanId",
+                table: "FixedDatesSchedulation",
+                column: "MailingPlanId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alert");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FixedDatesSchedulation_MailingPlanId",
+                table: "FixedDatesSchedulation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FixedDatesSchedulation_MailingPlanId",
+                table: "FixedDatesSchedulation",
+                column: "MailingPlanId",
+                unique: true);
+        }
+    }
+}
