2c7d9a5e87fb8de83e935ada0d7028e54db5bdd9
zioIgna <ignaziocarbonaro@hotmail.com>
2024-06-15 18:55:03 +0200

    Impostazione pagina di gestione allegati #18

---
+++ FileToEmailLinker/Controllers/AllegatiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileToEmailLinker.Controllers
+{
+    public class AllegatiController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] allegati = { "file1", "file2", "file3", "file4" };
+            return View(allegati);
+        }
+    }
+}
